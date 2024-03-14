899cb77421068aea18c56184c35b31a9f7904fab
Khip01 <aakhifathallah24@gmail.com>
2024-03-14 10:06:57 +0700

    feat: implement dark mode
    implement dark mode switch functionaly

--- lib/pages/creation_page.dart
+++ lib/pages/creation_page.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:me/component/components.dart';
+import 'package:me/provider/theme_provider.dart';
 
-class CreationPage extends StatelessWidget {
+class CreationPage extends ConsumerWidget {
   const CreationPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final scrHeight = MediaQuery.sizeOf(context).height;
 
     return Scaffold(
       body: Container(
         height: scrHeight,
+        color: (ref.watch(isDarkMode) ? styleUtil.c_33 : styleUtil.c_255),
       ),
     );
   }
