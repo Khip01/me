aa48375cc09d535d1c3eb531744baaacbbee0bdb
Khip01 <aakhifathallah24@gmail.com>
2024-03-19 15:10:16 +0700

    chore: small changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -31,7 +32,7 @@ class MyApp extends ConsumerWidget {
       }),
     ],
     initialLocation: '/',
-    debugLogDiagnostics: true,
+    debugLogDiagnostics: kDebugMode,
   );
 
   MyApp({super.key});
