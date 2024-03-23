e6300c660e4810fe7237d4273f66ee948ef91187
Khip01 <aakhifathallah24@gmail.com>
2024-03-23 12:53:19 +0700

    chore: tidy up code

--- lib/utility/icon_util.dart
+++ lib/utility/icon_util.dart
@@ -1,22 +1,22 @@
 class IconUtil {
   // Welcome Page
-  String wavingHand = 'assets/icons/waving_hand.png';
+  final String wavingHand = 'assets/icons/waving_hand.png';
   // Further Page
-  String flutterLogo = 'assets/icons/flutter_16.png';
+  final String flutterLogo = 'assets/icons/flutter_16.png';
   // Icons Social Media
-  String imgGithubDefault = "assets/icons/github/github_default.png"; // Github
-  String imgGithubDark = "assets/icons/github/github_hover_dark.png";
-  String imgGithubLight = "assets/icons/github/github_hover_light.png";
-  String imgInstagramDefault = "assets/icons/instagram/instagram_default.png"; // Instagram
-  String imgInstagramDark = "assets/icons/instagram/instagram_hover_dark.png";
-  String imgInstagramLight = "assets/icons/instagram/instagram_hover_light.png";
-  String imgFacebookDefault = "assets/icons/facebook/facebook_default.png"; // Facebook
-  String imgFacebookDark = "assets/icons/facebook/facebook_hover_dark.png";
-  String imgFacebookLight = "assets/icons/facebook/facebook_hover_light.png";
-  String imgGmailDefault = "assets/icons/gmail/gmail_default.png"; // Gmail
-  String imgGmailDark = "assets/icons/gmail/gmail_hover_dark.png";
-  String imgGmailLight = "assets/icons/gmail/gmail_hover_light.png";
-  String imgLinkedinDefault = "assets/icons/linkedin/linkedin_default.png"; // LinkedIn
-  String imgLinkedinDark = "assets/icons/linkedin/linkedin_hover_dark.png";
-  String imgLinkedinLight = "assets/icons/linkedin/linkedin_hover_light.png";
+  final String imgGithubDefault = "assets/icons/github/github_default.png"; // Github
+  final String imgGithubDark = "assets/icons/github/github_hover_dark.png";
+  final String imgGithubLight = "assets/icons/github/github_hover_light.png";
+  final String imgInstagramDefault = "assets/icons/instagram/instagram_default.png"; // Instagram
+  final String imgInstagramDark = "assets/icons/instagram/instagram_hover_dark.png";
+  final String imgInstagramLight = "assets/icons/instagram/instagram_hover_light.png";
+  final String imgFacebookDefault = "assets/icons/facebook/facebook_default.png"; // Facebook
+  final String imgFacebookDark = "assets/icons/facebook/facebook_hover_dark.png";
+  final String imgFacebookLight = "assets/icons/facebook/facebook_hover_light.png";
+  final String imgGmailDefault = "assets/icons/gmail/gmail_default.png"; // Gmail
+  final String imgGmailDark = "assets/icons/gmail/gmail_hover_dark.png";
+  final String imgGmailLight = "assets/icons/gmail/gmail_hover_light.png";
+  final String imgLinkedinDefault = "assets/icons/linkedin/linkedin_default.png"; // LinkedIn
+  final String imgLinkedinDark = "assets/icons/linkedin/linkedin_hover_dark.png";
+  final String imgLinkedinLight = "assets/icons/linkedin/linkedin_hover_light.png";
 }
