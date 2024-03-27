ded20b48a86957e6a79e20a06023cfcbdf611999
Khip01 <aakhifathallah24@gmail.com>
2024-03-27 10:02:50 +0700

    add: init firebase
    init firebase for the first time

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -8,8 +9,11 @@ import 'package:me/pages/pages.dart';
 import 'package:me/transition_setting/default_transition_page.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform); // Firebase Init
   await preloadImage();
   runApp(ProviderScope(child: MyApp()));
 }
