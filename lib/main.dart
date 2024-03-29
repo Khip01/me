57ffe6b586b2e12307a9262be09daa1581684fac
Khip01 <aakhifathallah24@gmail.com>
2024-03-29 09:26:27 +0700

    fix: path to anonym page

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:go_router/go_router.dart';
 import 'package:me/helper/preload_image.dart';
 import 'package:me/pages/not_found_page.dart';
 import 'package:me/pages/pages.dart';
-import 'super_user/super_user.dart';
+import 'package:me/super_user/super_user.dart';
 import 'package:me/transition_setting/default_transition_page.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 
@@ -21,7 +21,7 @@ Future<void> main() async {
 
 class MyApp extends ConsumerWidget {
   final GoRouter _router = GoRouter(
-    errorBuilder: (context, state){
+    errorBuilder: (context, state) {
       return const NotFoundPage();
     },
     routes: [
@@ -37,18 +37,9 @@ class MyApp extends ConsumerWidget {
       GoRoute(path: "/further", name: "further", pageBuilder: (context, state){
         return buildPageWithDefaultTransition(context: context, state: state, child: const FurtherPage());
       }),
-      GoRoute(
-          path: "/super-user",
-          name: "super-user-login",
-          routes: [
-            GoRoute(path: "master", name: "super-user-page", pageBuilder: (context, state){
-              return buildPageWithDefaultTransition(context: context, state: state, child: const SuperUserPage());
-            }),
-          ],
-          pageBuilder: (context, state){
-            return buildPageWithDefaultTransition(context: context, state: state, child: const SuperUserLoginPage());
-          },
-      ),
+      GoRoute(path: "/super-user", name: "super-user-login", pageBuilder: (context, state) {
+        return buildPageWithDefaultTransition(context: context, state: state, child: const SuperUserBase());
+      }),
     ],
     initialLocation: '/',
     debugLogDiagnostics: kDebugMode,
@@ -78,7 +69,8 @@ class MyApp extends ConsumerWidget {
                 Breakpoint(start: 801, end: 1100, name: 'DESKTOP-MD'),
                 Breakpoint(start: 1101, end: 1920, name: 'DESKTOP-LG'),
                 Breakpoint(start: 1921, end: double.infinity, name: '4K'),
-              ]),
+              ],
+          ),
       routerConfig: _router,
       debugShowCheckedModeBanner: false, // Remove Debug banner when debugging
     );
