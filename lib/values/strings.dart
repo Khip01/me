3700be4fcd52678e951c9b5af67cbc5ffda03294
Khip01 <aakhifathallah24@gmail.com>
2024-05-14 11:58:05 +0700

    refactor: add const string from local data

---
+++ lib/values/strings.dart
@@ -0,0 +1,217 @@
+part of 'values.dart';
+
+class StringConst {
+  // PROJECTS
+  // CAFE APP - KHIP CAFE
+  static const String CAFEAPP_KHIPCAFE_PROJECT_NAME = "CafeApp - KhipCafe C#";
+  static const String CAFEAPP_KHIPCAFE_PROJECT_DESCRIPTION = "Cafe application that operates in the Desktop field. Has 3 access rights, namely Admin, Cashier, and Manager. Interactive and good user interface design.";
+  static const String CAFEAPP_KHIPCAFE_ADDITIONAL_LINK = "https://github.com/Khip01/CafeApp-KhipCafe/tree/main/Executetable%20App";
+  static const String CAFEAPP_KHIPCAFE_ADDITIONAL_LINK_DESCRIPTION = "installer";
+  static const String CAFEAPP_KHIPCAFE_CREATOR_GITHUB_LINK = "https://github.com/Khip01";
+  static const List<String> CAFEAPP_KHIPCAFE_CREATOR_NAME = [
+    "Akhmad Aakhif A"
+  ];
+  static const int CAFEAPP_KHIPCAFE_DATE_PROJECT_CREATED = 1679418000000;
+  static const List<String> CAFEAPP_KHIPCAFE_PROJECT_CATEGORIES = [
+    "c#",
+    "desktop-app",
+    "winform",
+    "sql-server"
+  ];
+  static const List<String> CAFEAPP_KHIPCAFE_PROJECT_LINK_TO_GITHUB = [
+    "https://github.com/Khip01/CafeApp-KhipCafe"
+  ];
+  static const String CAFEAPP_KHIPCAFE_PROJECT_LINK_TO_DEMO_WEB = "";
+  // SPP PAYMENT - SPP PAY V2 C#
+  static const String SPPPAYMENT_SPPPAY_V2_PROJECT_NAME = "SPP Payment - SPPay V2 C#";
+  static const String SPPPAYMENT_SPPPAY_V2_PROJECT_DESCRIPTION = "SPP Payment Application engaged in the Desktop field. Has 3 access rights, namely Admin, Officer, and Student. Interactive and good user interface design.";
+  static const String SPPPAYMENT_SPPPAY_V2_ADDITIONAL_LINK = "";
+  static const String SPPPAYMENT_SPPPAY_V2_ADDITIONAL_LINK_DESCRIPTION = "";
+  static const String SPPPAYMENT_SPPPAY_V2_CREATOR_GITHUB_LINK = "https://github.com/Khip01";
+  static const List<String> SPPPAYMENT_SPPPAY_V2_CREATOR_NAME = [
+    "Akhmad Aakhif A"
+  ];
+  static const int SPPPAYMENT_SPPPAY_V2_DATE_PROJECT_CREATED = 1671123600000;
+  static const List<String> SPPPAYMENT_SPPPAY_V2_PROJECT_CATEGORIES = [
+    "c#",
+    "desktop-app",
+    "winform",
+    "sql-server"
+  ];
+  static const List<String> SPPPAYMENT_SPPPAY_V2_PROJECT_LINK_TO_GITHUB = [
+    "https://github.com/Khip01/SPP-Pay-App"
+  ];
+  static const String SPPPAYMENT_SPPPAY_V2_PROJECT_LINK_TO_DEMO_WEB = "";
+  // ROCK PAPER SCISSORS GAME C++
+  static const String ROCK_PAPPER_SCISSORS_GAME_PROJECT_NAME = "Rock Paper Scissors Game C++";
+  static const String ROCK_PAPPER_SCISSORS_GAME_PROJECT_DESCRIPTION = "Just a simple and FUN rock, paper, scissors game using C++";
+  static const String ROCK_PAPPER_SCISSORS_GAME_ADDITIONAL_LINK = "https://github.com/Khip01/Batu-Kertas-Gunting/blob/main/BatuKertasGunting.exe";
+  static const String ROCK_PAPPER_SCISSORS_GAME_ADDITIONAL_LINK_DESCRIPTION = "executable";
+  static const String ROCK_PAPPER_SCISSORS_GAME_CREATOR_GITHUB_LINK = "https://github.com/Khip01";
+  static const List<String> ROCK_PAPPER_SCISSORS_GAME_CREATOR_NAME = [
+    "Akhmad Aakhif A"
+  ];
+  static const int ROCK_PAPPER_SCISSORS_GAME_DATE_PROJECT_CREATED = 1607446800000;
+  static const List<String> ROCK_PAPPER_SCISSORS_GAME_PROJECT_CATEGORIES = [
+    "c++",
+    "cli",
+    "game",
+    "simple"
+  ];
+  static const List<String> ROCK_PAPPER_SCISSORS_GAME_PROJECT_LINK_TO_GITHUB = [
+    "https://github.com/Khip01/Batu-Kertas-Gunting"
+  ];
+  static const String ROCK_PAPPER_SCISSORS_GAME_PROJECT_LINK_TO_DEMO_WEB = "";
+  // SPP PAYMENT - SPP PAY JAVA
+  static const String SPPPAYMENT_SPPPAY_PROJECT_NAME = "SPP Payment - SPPay Java";
+  static const String SPPPAYMENT_SPPPAY_PROJECT_DESCRIPTION = "SPP Payment Application engaged in the Desktop field. Has 3 access rights, namely Admin, Officer, and Student. Less interactive and user interface design.";
+  static const String SPPPAYMENT_SPPPAY_ADDITIONAL_LINK = "";
+  static const String SPPPAYMENT_SPPPAY_ADDITIONAL_LINK_DESCRIPTION = "";
+  static const String SPPPAYMENT_SPPPAY_CREATOR_GITHUB_LINK = "https://github.com/Khip01";
+  static const List<String> SPPPAYMENT_SPPPAY_CREATOR_NAME = [
+    "Akhmad Aakhif A"
+  ];
+  static const int SPPPAYMENT_SPPPAY_DATE_PROJECT_CREATED = 1664125200000;
+  static const List<String> SPPPAYMENT_SPPPAY_PROJECT_CATEGORIES = [
+    "java",
+    "desktop-app",
+    "jframe-form",
+    "mysql"
+  ];
+  static const List<String> SPPPAYMENT_SPPPAY_PROJECT_LINK_TO_GITHUB = [
+    "https://github.com/Khip01/SPP-Pay-App-Java"
+  ];
+  static const String SPPPAYMENT_SPPPAY_PROJECT_LINK_TO_DEMO_WEB = "";
+  // MY FIRST WEB PORTFOLIO
+  static const String FIRST_WEB_PORTFOLIO_PROJECT_NAME = "My First Web Portfolio";
+  static const String FIRST_WEB_PORTFOLIO_PROJECT_DESCRIPTION = "My first web portfolio which contains my personal data. Using native websites.";
+  static const String FIRST_WEB_PORTFOLIO_ADDITIONAL_LINK = "";
+  static const String FIRST_WEB_PORTFOLIO_ADDITIONAL_LINK_DESCRIPTION = "";
+  static const String FIRST_WEB_PORTFOLIO_CREATOR_GITHUB_LINK = "https://github.com/Khip01";
+  static const List<String> FIRST_WEB_PORTFOLIO_CREATOR_NAME = [
+    "Akhmad Aakhif A"
+  ];
+  static const int FIRST_WEB_PORTFOLIO_DATE_PROJECT_CREATED = 1629997200000;
+  static const List<String> FIRST_WEB_PORTFOLIO_PROJECT_CATEGORIES = [
+    "native-web",
+    "portfolio",
+    "first"
+  ];
+  static const List<String> FIRST_WEB_PORTFOLIO_PROJECT_LINK_TO_GITHUB = [
+    "https://github.com/Khip01/Khip01.github.io"
+  ];
+  static const String FIRST_WEB_PORTFOLIO_PROJECT_LINK_TO_DEMO_WEB = "https://khip01.github.io/";
+  // CALCULATOR GUI JAVA
+  static const String CALCULATOR_GUI_JAVA_PROJECT_NAME = "Calculator GUI Java";
+  static const String CALCULATOR_GUI_JAVA_PROJECT_DESCRIPTION = "Contains the calculator desktop application.";
+  static const String CALCULATOR_GUI_JAVA_ADDITIONAL_LINK = "";
+  static const String CALCULATOR_GUI_JAVA_ADDITIONAL_LINK_DESCRIPTION = "";
+  static const String CALCULATOR_GUI_JAVA_CREATOR_GITHUB_LINK = "https://github.com/Khip01";
+  static const List<String> CALCULATOR_GUI_JAVA_CREATOR_NAME = [
+    "Akhmad Aakhif A"
+  ];
+  static const int CALCULATOR_GUI_JAVA_DATE_PROJECT_CREATED = 1611766800000;
+  static const List<String> CALCULATOR_GUI_JAVA_PROJECT_CATEGORIES = [
+    "java",
+    "desktop-app",
+    "jframe-form",
+    "calculator",
+    "simple"
+  ];
+  static const List<String> CALCULATOR_GUI_JAVA_PROJECT_LINK_TO_GITHUB = [
+    "https://github.com/Khip01/Kalkulator-GUI-Java"
+  ];
+  static const String CALCULATOR_GUI_JAVA_PROJECT_LINK_TO_DEMO_WEB = "";
+  // KALKULATOR BASIC C++
+  static const String KALKULATOR_BASIC_CPP_PROJECT_NAME = "Kalkulator Basic C++";
+  static const String KALKULATOR_BASIC_CPP_PROJECT_DESCRIPTION = "A simple calculator tool used to calculate (Addition, Subtraction, Multiplication, Division and Modulus).";
+  static const String KALKULATOR_BASIC_CPP_ADDITIONAL_LINK = "https://github.com/Khip01/Kalkulator-Cpp/blob/main/Kalkulator.exe";
+  static const String KALKULATOR_BASIC_CPP_ADDITIONAL_LINK_DESCRIPTION = "executable";
+  static const String KALKULATOR_BASIC_CPP_CREATOR_GITHUB_LINK = "https://github.com/Khip01";
+  static const List<String> KALKULATOR_BASIC_CPP_CREATOR_NAME = [
+    "Akhmad Aakhif A"
+  ];
+  static const int KALKULATOR_BASIC_CPP_DATE_PROJECT_CREATED = 1596474000000;
+  static const List<String> KALKULATOR_BASIC_CPP_PROJECT_CATEGORIES = [
+    "c++",
+    "cli",
+    "calculator",
+    "simple"
+  ];
+  static const List<String> KALKULATOR_BASIC_CPP_PROJECT_LINK_TO_GITHUB = [
+    "https://github.com/Khip01/Kalkulator-Cpp"
+  ];
+  static const String KALKULATOR_BASIC_CPP_PROJECT_LINK_TO_DEMO_WEB = "";
+  // FLUTTER PORTFOLIO
+  static const String FLUTTER_PORTFOLIO_PROJECT_NAME = "Flutter Portfolio";
+  static const String FLUTTER_PORTFOLIO_PROJECT_DESCRIPTION = "Create a simple portfolio using flutter web and firebase.";
+  static const String FLUTTER_PORTFOLIO_ADDITIONAL_LINK = "";
+  static const String FLUTTER_PORTFOLIO_ADDITIONAL_LINK_DESCRIPTION = "";
+  static const String FLUTTER_PORTFOLIO_CREATOR_GITHUB_LINK = "https://github.com/Khip01";
+  static const List<String> FLUTTER_PORTFOLIO_CREATOR_NAME = [
+    "Akhmad Aakhif A"
+  ];
+  static const int FLUTTER_PORTFOLIO_DATE_PROJECT_CREATED = 1708794000000;
+  static const List<String> FLUTTER_PORTFOLIO_PROJECT_CATEGORIES = [
+    "flutter",
+    "web-app",
+    "firebase"
+  ];
+  static const List<String> FLUTTER_PORTFOLIO_PROJECT_LINK_TO_GITHUB = [
+    "https://github.com/Khip01/me"
+  ];
+  static const String FLUTTER_PORTFOLIO_PROJECT_LINK_TO_DEMO_WEB = "https://khip01.github.io/me";
+  static const String FLUTTER_PORTFOLIO_HIGHLIGHT_DESCRIPTION = "Explore what I've been working on lately";
+  static const String FLUTTER_PORTFOLIO_HIGHLIGHT_HEADER = "Projects that have recently been created";
+  static const String FLUTTER_PORTFOLIO_HIGHLIGHT_TOPIC = "Recently";
+  // COMMENT SECTION WEB
+  static const String COMMENT_SECTION_WEB_PROJECT_NAME = "Comment Section Web";
+  static const String COMMENT_SECTION_WEB_PROJECT_DESCRIPTION = "Create a Simple Chat Web Page and Mobile App that can be used anonymously for everyone! Using Flutter Web technology and Firebase Realtime Database.";
+  static const String COMMENT_SECTION_WEB_ADDITIONAL_LINK = "https://github.com/Khip01/comment_section/releases";
+  static const String COMMENT_SECTION_WEB_ADDITIONAL_LINK_DESCRIPTION = "release-mobile-app";
+  static const String COMMENT_SECTION_WEB_CREATOR_GITHUB_LINK = "https://github.com/Khip01";
+  static const List<String> COMMENT_SECTION_WEB_CREATOR_NAME = [
+    "Akhmad Aakhif A"
+  ];
+  static const int COMMENT_SECTION_WEB_DATE_PROJECT_CREATED = 1707411600000;
+  static const List<String> COMMENT_SECTION_WEB_PROJECT_CATEGORIES = [
+    "flutter",
+    "web-app",
+    "mobile-app",
+    "firebase"
+  ];
+  static const List<String> COMMENT_SECTION_WEB_PROJECT_LINK_TO_GITHUB = [
+    "https://github.com/Khip01/comment_section"
+  ];
+  static const String COMMENT_SECTION_WEB_PROJECT_LINK_TO_DEMO_WEB = "https://khip01.github.io/comment_section";
+  static const String COMMENT_SECTION_WEB_HIGHLIGHT_DESCRIPTION = "Projects that use technology that I am interested in exploring";
+  static const String COMMENT_SECTION_WEB_HIGHLIGHT_HEADER = "Firebase?! Light up my brother";
+  static const String COMMENT_SECTION_WEB_HIGHLIGHT_TOPIC = "Currently being explored";
+  // RESTAURANT_KHIP01
+  static const String RESTAURANT_KHIP01_PROJECT_NAME = "Restaurant_Khip01";
+  static const String RESTAURANT_KHIP01_PROJECT_DESCRIPTION = "Create Simple RESTful API with GO, Complete with simple Mobile and Desktop App";
+  static const String RESTAURANT_KHIP01_ADDITIONAL_LINK = "https://github.com/Khip01/Restaurant_Khip01/releases";
+  static const String RESTAURANT_KHIP01_ADDITIONAL_LINK_DESCRIPTION = "release-app";
+  static const String RESTAURANT_KHIP01_CREATOR_GITHUB_LINK = "https://github.com/Khip01";
+  static const List<String> RESTAURANT_KHIP01_CREATOR_NAME = [
+    "Akhmad Aakhif A"
+  ];
+  static const int RESTAURANT_KHIP01_DATE_PROJECT_CREATED = 1702054800000;
+  static const List<String> RESTAURANT_KHIP01_PROJECT_CATEGORIES = [
+    "flutter",
+    "mobile-app",
+    "c#",
+    "desktop-app",
+    "go",
+    "restful-api",
+    "sql-server",
+    "crud"
+  ];
+  static const List<String> RESTAURANT_KHIP01_PROJECT_LINK_TO_GITHUB = [
+    "https://github.com/Khip01/Restaurant_Khip01"
+  ];
+  static const String RESTAURANT_KHIP01_PROJECT_LINK_TO_DEMO_WEB = "";
+  static const String RESTAURANT_KHIP01_HIGHLIGHT_DESCRIPTION = "Not quite related, but part of my journey";
+  static const String RESTAURANT_KHIP01_HIGHLIGHT_HEADER = "My stepping stone";
+  static const String RESTAURANT_KHIP01_HIGHLIGHT_TOPIC = "Part of My Journey";
+}
