224406f79455109386cb639b53e7e501415cf40e
Khip01 <aakhifathallah24@gmail.com>
2024-05-14 11:58:36 +0700

    refactor: add const images path string from local data

---
+++ lib/values/images.dart
@@ -0,0 +1,47 @@
+part of 'values.dart';
+
+class ImagePath {
+  static const String projectDir = 'assets/images/projects';
+  static const String creatorPhotoDir = 'assets/images/creators_photo';
+
+  // ---------- IMAGES CREATOR PATH ----------
+  static const String KHIP01_PHOTO_PROFILE = '$creatorPhotoDir/khip01_profile.png';
+
+  // ---------- IMAGES PROJECT PATH ----------
+  // CAFE APP - KHIP CAFE
+  static const String CAFEAPP_KHIPCAFE_C = '$projectDir/cafeapp_khipcafe/cafeapp_khipcafe_cover.jpg';
+
+  // CALCULATOR GUI JAVA
+  static const String CALCULATOR_GUI_JAVA_C = '$projectDir/calculator_gui_java/calculator_gui_java_cover.png';
+
+  // COMMENT SECTION WEB
+  static const String COMMENT_SECTION_WEB_C = '$projectDir/comment_section_web/comment_section_web_cover.png';
+
+  // FIRST WEB PORTFOLIO
+  static const String FIRST_WEB_PORTFOLIO_C = '$projectDir/first_web_portfolio/first_web_portfo_cover.png';
+  static const String FIRST_WEB_PORTFOLIO_2 = '$projectDir/first_web_portfolio/first_web_portfo_2.png';
+  static const String FIRST_WEB_PORTFOLIO_3 = '$projectDir/first_web_portfolio/first_web_portfo_3.png';
+
+  // FLUTTER PORTFOLIO
+  static const String FLUTTER_PORTFOLIO_C = '$projectDir/flutter-portfolio/flutter-portfolio_cover.png';
+
+  // KALKULATOR BASIC CPP
+  static const String KALKULATOR_BASIC_CPP_C = '$projectDir/kalkulator_basic_cpp/kalkulator_basic_cpp.png';
+
+  // RESTAURANT KHIP01 C#
+  static const String RESTAURANT_KHIP01_C = '$projectDir/restaurant_khip01/restaurant_khip01_cover.png';
+
+  // ROCK PAPPER SCISSORS GAME CPP
+  static const String ROCK_PAPPER_SCISSORS_GAME_C = '$projectDir/rock_papper_scissors_game/rock_papper_scissors_game_cover.png';
+  static const String ROCK_PAPPER_SCISSORS_GAME_2 = '$projectDir/rock_papper_scissors_game/rock_papper_scissors_game_2.png';
+
+  // SPP PAYMENT - SPP PAY JAVA
+  static const String SPPPAYMENT_SPPPAY_C = '$projectDir/spppayment_spppay/spppayment_spppay_cover.jpg';
+  static const String SPPPAYMENT_SPPPAY_2 = '$projectDir/spppayment_spppay/spppayment_spppay_2.jpg';
+
+  // SPP PAYMENT - SPP PAY V2 C#
+  static const String SPPPAYMENT_SPPPAY_V2_C = '$projectDir/spppayment_spppay_v2/spppayment_spppay_v2_cover.jpg';
+  static const String SPPPAYMENT_SPPPAY_V2_2 = '$projectDir/spppayment_spppay_v2/spppayment_spppay_v2_2.jpg';
+  static const String SPPPAYMENT_SPPPAY_V2_3 = '$projectDir/spppayment_spppay_v2/spppayment_spppay_v2_3.jpg';
+
+}
