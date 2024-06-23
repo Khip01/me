4eb6d969b90e66fe5135252f73adfb2b5b5213ef
Khip01 <aakhifathallah24@gmail.com>
2024-06-23 17:17:05 +0700

    chore: tidy up

--- lib/pages/creation_detail_page.dart
+++ lib/pages/creation_detail_page.dart
@@ -488,86 +488,89 @@ class _ImagePreviewState extends ConsumerState<ImagePreview> {
                     padding: contentCardPadding(context),
                     child: GestureDetector(
                       onTap: () {}, // Prevent the outer GestureDetector from closing the preview,
-                      child: SizedBox(
-                        width: MediaQuery.sizeOf(context).width,
-                        child: Align(
-                          alignment: Alignment.center,
-                          child: Container(
-                            margin: const EdgeInsets.only(bottom: 10),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(getIsMobileSize(context) ? 0 : 30),
-                              color: (ref.watch(isDarkMode))
-                                  ? _styleUtil.c_33
-                                  : _styleUtil.c_255,
-                              boxShadow: [
-                                BoxShadow(
-                                  color: (ref.watch(isDarkMode))
-                                      ? const Color.fromARGB(255, 61, 61, 61)
-                                      : const Color.fromARGB(255, 203, 203, 203),
-                                  blurRadius: 80.0,
-                                ),
-                              ],
-                            ),
-                            width: 100,
-                            height: 50,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                IgnorePointer(
-                                  ignoring: ref.read(isPreviewMode) == 0 ? true : false,
-                                  child: AnimatedOpacity(
-                                    duration: const Duration(milliseconds: 100),
-                                    opacity: ref.read(isPreviewMode) == 0 ? 0 : 1,
-                                    curve: Curves.easeInOut,
-                                    child: TextHighlightDecider(
-                                      isCompactMode: getIsMobileSize(context) || getIsTabletSize(context),
-                                      colorStart: (ref.watch(isDarkMode)) ? _styleUtil.c_170 : _styleUtil.c_61,
-                                      colorEnd: (ref.watch(isDarkMode)) ? _styleUtil.c_255 : _styleUtil.c_24,
-                                      actionDelay: Duration(milliseconds: (getIsMobileSize(context) || getIsTabletSize(context)) ? 500 : 100),
-                                      additionalOnTapAction: () {
-                                        if(ref.read(isPreviewMode) != null && ref.read(isPreviewMode)! > 0){
-                                          int currentIndex = ref.read(isPreviewMode)!;
-                                          ref.read(isPreviewMode.notifier).state = (currentIndex - 1);
-                                        }
-                                      },
-                                      builder: (color) {
-                                        return Icon(
-                                          Icons.keyboard_arrow_left_rounded,
-                                          size: 33,
-                                          color: color,
-                                        );
-                                      },
+                      child: Visibility(
+                        visible: widget.images.length > 1 ? true : false,
+                        child: SizedBox(
+                          width: MediaQuery.sizeOf(context).width,
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Container(
+                              margin: const EdgeInsets.only(bottom: 10),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(getIsMobileSize(context) ? 0 : 30),
+                                color: (ref.watch(isDarkMode))
+                                    ? _styleUtil.c_33
+                                    : _styleUtil.c_255,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: (ref.watch(isDarkMode))
+                                        ? const Color.fromARGB(255, 61, 61, 61)
+                                        : const Color.fromARGB(255, 203, 203, 203),
+                                    blurRadius: 80.0,
+                                  ),
+                                ],
+                              ),
+                              width: 100,
+                              height: 50,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  IgnorePointer(
+                                    ignoring: ref.read(isPreviewMode) == 0 ? true : false,
+                                    child: AnimatedOpacity(
+                                      duration: const Duration(milliseconds: 100),
+                                      opacity: ref.read(isPreviewMode) == 0 ? 0 : 1,
+                                      curve: Curves.easeInOut,
+                                      child: TextHighlightDecider(
+                                        isCompactMode: getIsMobileSize(context) || getIsTabletSize(context),
+                                        colorStart: (ref.watch(isDarkMode)) ? _styleUtil.c_170 : _styleUtil.c_61,
+                                        colorEnd: (ref.watch(isDarkMode)) ? _styleUtil.c_255 : _styleUtil.c_24,
+                                        actionDelay: Duration(milliseconds: (getIsMobileSize(context) || getIsTabletSize(context)) ? 500 : 100),
+                                        additionalOnTapAction: () {
+                                          if(ref.read(isPreviewMode) != null && ref.read(isPreviewMode)! > 0){
+                                            int currentIndex = ref.read(isPreviewMode)!;
+                                            ref.read(isPreviewMode.notifier).state = (currentIndex - 1);
+                                          }
+                                        },
+                                        builder: (color) {
+                                          return Icon(
+                                            Icons.keyboard_arrow_left_rounded,
+                                            size: 33,
+                                            color: color,
+                                          );
+                                        },
+                                      ),
                                     ),
                                   ),
-                                ),
-                                IgnorePointer(
-                                  ignoring: ref.read(isPreviewMode) == widget.images.length-1 ? true : false,
-                                  child: AnimatedOpacity(
-                                    duration: const Duration(milliseconds: 100),
-                                    opacity: ref.read(isPreviewMode) == widget.images.length-1 ? 0 : 1,
-                                    curve: Curves.easeInOut,
-                                    child: TextHighlightDecider(
-                                      isCompactMode: getIsMobileSize(context) || getIsTabletSize(context),
-                                      colorStart: (ref.watch(isDarkMode)) ? _styleUtil.c_170 : _styleUtil.c_61,
-                                      colorEnd: (ref.watch(isDarkMode)) ? _styleUtil.c_255 : _styleUtil.c_24,
-                                      actionDelay: Duration(milliseconds: (getIsMobileSize(context) || getIsTabletSize(context)) ? 500 : 100),
-                                      additionalOnTapAction: () {
-                                        if(ref.read(isPreviewMode) != null && ref.read(isPreviewMode)! < widget.images.length-1){
-                                          int currentIndex = ref.read(isPreviewMode)!;
-                                          ref.read(isPreviewMode.notifier).state = (currentIndex + 1);
-                                        }
-                                      },
-                                      builder: (color) {
-                                        return Icon(
-                                          Icons.keyboard_arrow_right_rounded,
-                                          size: 33,
-                                          color: color,
-                                        );
-                                      },
+                                  IgnorePointer(
+                                    ignoring: ref.read(isPreviewMode) == widget.images.length-1 ? true : false,
+                                    child: AnimatedOpacity(
+                                      duration: const Duration(milliseconds: 100),
+                                      opacity: ref.read(isPreviewMode) == widget.images.length-1 ? 0 : 1,
+                                      curve: Curves.easeInOut,
+                                      child: TextHighlightDecider(
+                                        isCompactMode: getIsMobileSize(context) || getIsTabletSize(context),
+                                        colorStart: (ref.watch(isDarkMode)) ? _styleUtil.c_170 : _styleUtil.c_61,
+                                        colorEnd: (ref.watch(isDarkMode)) ? _styleUtil.c_255 : _styleUtil.c_24,
+                                        actionDelay: Duration(milliseconds: (getIsMobileSize(context) || getIsTabletSize(context)) ? 500 : 100),
+                                        additionalOnTapAction: () {
+                                          if(ref.read(isPreviewMode) != null && ref.read(isPreviewMode)! < widget.images.length-1){
+                                            int currentIndex = ref.read(isPreviewMode)!;
+                                            ref.read(isPreviewMode.notifier).state = (currentIndex + 1);
+                                          }
+                                        },
+                                        builder: (color) {
+                                          return Icon(
+                                            Icons.keyboard_arrow_right_rounded,
+                                            size: 33,
+                                            color: color,
+                                          );
+                                        },
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
                           ),
                         ),
