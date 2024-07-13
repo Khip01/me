35ab877a6b636bcef96a1dd8801a644353b9323d
Khip01 <aakhifathallah24@gmail.com>
2024-07-14 00:36:18 +0700

    chore: fix typo asset name/path

--- lib/values/images.dart
+++ lib/values/images.dart
@@ -2,7 +2,7 @@ part of 'values.dart';
 
 class ImagePath {
   static const String projectDir = 'assets/images/projects';
-  static const String certificateDir = 'assets.images/certificates';
+  static const String certificateDir = 'assets/images/certificates';
   static const String creatorPhotoDir = 'assets/images/creators_photo';
 
   // ---------- IMAGES CREATOR PATH ----------
@@ -47,22 +47,22 @@ class ImagePath {
 
   // ---------- IMAGES CERTIFICATE PATH ----------
   // Learn to Make Flutter Apps for Beginners
-  static const String DICODING_LEARN_TO_MAKE_FLUTTER_APPS_FOR_BEGINEERS_01 = '$certificateDir/dicoding_learn_to_make_flutter_apps_for_beginners_01.jpg';
-  static const String DICODING_LEARN_TO_MAKE_FLUTTER_APPS_FOR_BEGINEERS_02 = '$certificateDir/dicoding_learn_to_make_flutter_apps_for_beginners_02.jpg';
+  static const String DICODING_LEARN_TO_MAKE_FLUTTER_APPS_FOR_BEGINEERS_01 = '$certificateDir/dicoding_learn_to_make_flutter_apps_for_beginners/dicoding_learn_to_make_flutter_apps_for_beginners_01.jpg';
+  static const String DICODING_LEARN_TO_MAKE_FLUTTER_APPS_FOR_BEGINEERS_02 = '$certificateDir/dicoding_learn_to_make_flutter_apps_for_beginners/dicoding_learn_to_make_flutter_apps_for_beginners_02.jpg';
   // Getting Started Programming with Dart
-  static const String DICODING_GETTING_STARTED_PROGRAMMING_WITH_DART_01 = '$certificateDir/dicoding_getting_started_programming_with_dart_01.jpg';
-  static const String DICODING_GETTING_STARTED_PROGRAMMING_WITH_DART_02 = '$certificateDir/dicoding_getting_started_programming_with_dart_02.jpg';
-  static const String DICODING_GETTING_STARTED_PROGRAMMING_WITH_DART_03 = '$certificateDir/dicoding_getting_started_programming_with_dart_03.jpg';
+  static const String DICODING_GETTING_STARTED_PROGRAMMING_WITH_DART_01 = '$certificateDir/dicoding_getting_started_programming_with_dart/dicoding_getting_started_programming_with_dart_01.jpg';
+  static const String DICODING_GETTING_STARTED_PROGRAMMING_WITH_DART_02 = '$certificateDir/dicoding_getting_started_programming_with_dart/dicoding_getting_started_programming_with_dart_02.jpg';
+  static const String DICODING_GETTING_STARTED_PROGRAMMING_WITH_DART_03 = '$certificateDir/dicoding_getting_started_programming_with_dart/dicoding_getting_started_programming_with_dart_03.jpg';
   // Test of English for International Communication - TOEIC [DITAHAN]
   // static const String
   // IT Softwre Solution for Business - Lomba Kompetensi Siswa
-  static const String IT_SOFTWARE_SOLUTIONS_FOR_BUSINESS_01 = '$certificateDir/it_software_solutions_for_business.jpg';
+  static const String IT_SOFTWARE_SOLUTIONS_FOR_BUSINESS_01 = '$certificateDir/it_software_solutions_for_business/it_software_solutions_for_business_01.jpg';
   // Competency Test for Software Engineering Expertise - Sertifikat Uji Kompetensi
-  static const String COMPETENCY_TEST_FOR_SOFTWARE_ENGINEERING_EXPERTISE_01 = '$certificateDir/competency_test_for_software_engineering_expertise_01.jpg';
-  static const String COMPETENCY_TEST_FOR_SOFTWARE_ENGINEERING_EXPERTISE_02 = '$certificateDir/competency_test_for_software_engineering_expertise_02.jpg';
+  static const String COMPETENCY_TEST_FOR_SOFTWARE_ENGINEERING_EXPERTISE_01 = '$certificateDir/competency_test_for_software_engineering_expertise/competency_test_for_software_engineering_expertise_01.jpg';
+  static const String COMPETENCY_TEST_FOR_SOFTWARE_ENGINEERING_EXPERTISE_02 = '$certificateDir/competency_test_for_software_engineering_expertise/competency_test_for_software_engineering_expertise_02.jpg';
   // Professional Certification Institute Exam - Lembaga Sertifikasi Profesi
-  static const String PROFESSIONAL_CERTIFICATION_INSTITUTE_EXAM_01 = '$certificateDir/professional_certification_institute_exam_01.jpg';
-  static const String PROFESSIONAL_CERTIFICATION_INSTITUTE_EXAM_02 = '$certificateDir/professional_certification_institute_exam_02.jpg';
+  static const String PROFESSIONAL_CERTIFICATION_INSTITUTE_EXAM_01 = '$certificateDir/professional_certification_institute_exam/professional_certification_institute_exam_01.jpg';
+  static const String PROFESSIONAL_CERTIFICATION_INSTITUTE_EXAM_02 = '$certificateDir/professional_certification_institute_exam/professional_certification_institute_exam_02.jpg';
   // Test of English for International Communication - TOEIC
-  static const String TEST_OF_ENGLISH_FOR_INTERNATIONAL_COMMUNICATION_01 = '$certificateDir/test_of_english_for_international_communication.jpg';
+  static const String TEST_OF_ENGLISH_FOR_INTERNATIONAL_COMMUNICATION_01 = '$certificateDir/test_of_english_for_international_communication/test_of_english_for_international_communication_01.jpg';
 }
