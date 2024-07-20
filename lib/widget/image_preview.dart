8f30a456eb83e0aea63f75a7fb3119e01800c42a
Khip01 <aakhifathallah24@gmail.com>
2024-07-20 22:09:29 +0700

    feat: create new file as reusable widget from refactored creation detail page

---
+++ lib/widget/image_preview.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:me/widget/text_highlight_decider.dart';
+
+import '../Utility/style_util.dart';
+import '../component/spacing.dart';
+import '../component/visible.dart';
+import '../provider/image_preview_provider.dart';
+import '../provider/theme_provider.dart';
+
+class ImagePreview extends ConsumerStatefulWidget {
+  final List<String> images;
+  final int? isPreviewMode;
+
+  const ImagePreview({
+    super.key,
+    required this.images,
+    required this.isPreviewMode,
+  });
+
+  @override
+  ConsumerState<ImagePreview> createState() => _ImagePreviewState();
+}
+
+class _ImagePreviewState extends ConsumerState<ImagePreview> {
+  // General
+  final StyleUtil _styleUtil = StyleUtil();
+
+  @override
+  Widget build(BuildContext context) {
+    // int? selectedIndexImagePreview = ref.watch(isPreviewMode);
+
+    return Visibility(
+      // visible: ref.watch(isPreviewMode) != null,
+      visible: widget.isPreviewMode != null,
+      child: GestureDetector(
+        onTap: () => ref.read(isPreviewMode.notifier).state = null,
+        child: Container(
+          width: MediaQuery.sizeOf(context).width,
+          height: MediaQuery.sizeOf(context).height,
+          color: _styleUtil.c_61.withOpacity(.7),
+          child: Stack(
+            children: [
+              InteractiveViewer(
+                minScale: 0.5, // Minimum scale (zoom out)
+                maxScale: 2.0, // Maximum scale (zoom in)
+                child: Center(
+                  child: GestureDetector(
+                    onTap: () {}, // Prevent the outer GestureDetector from closing the preview,
+                    // child: ref.watch(isPreviewMode) != null ?
+                    child: widget.isPreviewMode != null ?
+                    Image.asset(widget.images[widget.isPreviewMode!]) :
+                    const SizedBox(),
+                  ),
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: contentCardPadding(context),
+                    child: SizedBox(
+                      width: MediaQuery.sizeOf(context).width,
+                      height: 100,
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: TextHighlightDecider(
+                          isCompactMode: getIsMobileSize(context) || getIsTabletSize(context),
+                          colorStart: (ref.watch(isDarkMode)) ? _styleUtil.c_170 : _styleUtil.c_61,
+                          colorEnd: (ref.watch(isDarkMode)) ? _styleUtil.c_255 : _styleUtil.c_24,
+                          actionDelay: Duration(milliseconds: (getIsMobileSize(context) || getIsTabletSize(context)) ? 500 : 100),
+                          additionalOnTapAction: () => ref.read(isPreviewMode.notifier).state = null,
+                          builder: (color) {
+                            return Icon(
+                              Icons.close,
+                              size: 33,
+                              color: color,
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: contentCardPadding(context),
+                    child: GestureDetector(
+                      onTap: () {}, // Prevent the outer GestureDetector from closing the preview,
+                      child: Visibility(
+                        visible: widget.images.length > 1 ? true : false,
+                        child: SizedBox(
+                          width: MediaQuery.sizeOf(context).width,
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Container(
+                              margin: const EdgeInsets.only(bottom: 10),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(getIsMobileSize(context) ? 0 : 30),
+                                color: ((ref.watch(isDarkMode))
+                                    ? _styleUtil.c_33
+                                    : _styleUtil.c_255).withOpacity(.7),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: (ref.watch(isDarkMode))
+                                        ? const Color.fromARGB(255, 61, 61, 61)
+                                        : const Color.fromARGB(255, 203, 203, 203),
+                                    blurRadius: 80.0,
+                                  ),
+                                ],
+                              ),
+                              width: 100,
+                              height: 50,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  IgnorePointer(
+                                    ignoring: ref.read(isPreviewMode) == 0 ? true : false,
+                                    child: AnimatedOpacity(
+                                      duration: const Duration(milliseconds: 100),
+                                      opacity: ref.read(isPreviewMode) == 0 ? 0 : 1,
+                                      curve: Curves.easeInOut,
+                                      child: TextHighlightDecider(
+                                        isCompactMode: getIsMobileSize(context) || getIsTabletSize(context),
+                                        colorStart: (ref.watch(isDarkMode)) ? _styleUtil.c_170 : _styleUtil.c_61,
+                                        colorEnd: (ref.watch(isDarkMode)) ? _styleUtil.c_255 : _styleUtil.c_24,
+                                        actionDelay: Duration(milliseconds: (getIsMobileSize(context) || getIsTabletSize(context)) ? 0 : 100),
+                                        delayAfterAnimation: const Duration(milliseconds: 0),
+                                        additionalOnTapAction: () {
+                                          if(ref.read(isPreviewMode) != null && ref.read(isPreviewMode)! > 0){
+                                            int currentIndex = ref.read(isPreviewMode)!;
+                                            ref.read(isPreviewMode.notifier).state = (currentIndex - 1);
+                                          }
+                                        },
+                                        builder: (color) {
+                                          return Icon(
+                                            Icons.keyboard_arrow_left_rounded,
+                                            size: 33,
+                                            color: color.withOpacity(.7),
+                                          );
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                  IgnorePointer(
+                                    ignoring: ref.read(isPreviewMode) == widget.images.length-1 ? true : false,
+                                    child: AnimatedOpacity(
+                                      duration: const Duration(milliseconds: 100),
+                                      opacity: ref.read(isPreviewMode) == widget.images.length-1 ? 0 : 1,
+                                      curve: Curves.easeInOut,
+                                      child: TextHighlightDecider(
+                                        isCompactMode: getIsMobileSize(context) || getIsTabletSize(context),
+                                        colorStart: (ref.watch(isDarkMode)) ? _styleUtil.c_170 : _styleUtil.c_61,
+                                        colorEnd: (ref.watch(isDarkMode)) ? _styleUtil.c_255 : _styleUtil.c_24,
+                                        actionDelay: Duration(milliseconds: (getIsMobileSize(context) || getIsTabletSize(context)) ? 0 : 100),
+                                        delayAfterAnimation: const Duration(milliseconds: 0),
+                                        additionalOnTapAction: () {
+                                          if(ref.read(isPreviewMode) != null && ref.read(isPreviewMode)! < widget.images.length-1){
+                                            int currentIndex = ref.read(isPreviewMode)!;
+                                            ref.read(isPreviewMode.notifier).state = (currentIndex + 1);
+                                          }
+                                        },
+                                        builder: (color) {
+                                          return Icon(
+                                            Icons.keyboard_arrow_right_rounded,
+                                            size: 33,
+                                            color: color.withOpacity(.7),
+                                          );
+                                        },
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widget/list_image_section.dart
@@ -0,0 +1,227 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:me/widget/scroll_behavior.dart';
+
+import '../Utility/style_util.dart';
+import '../component/visible.dart';
+import '../provider/image_preview_provider.dart';
+import '../provider/theme_provider.dart';
+import 'highlighted_widget_on_hover.dart';
+
+
+class ListImageSection extends ConsumerStatefulWidget {
+  final List<String> images;
+  final double listViewHeight;
+  final double imageWidth;
+  final double? imageHeight;
+  final Function<Widget>(String image) childImageBuilder;
+  final Color? customBackgroundImageColor;
+  final EdgeInsets? listViewCustomPadding;
+  final Function()? customOnTapItem;
+
+  const ListImageSection({
+    super.key,
+    required this.images,
+    required this.listViewHeight,
+    required this.imageWidth,
+    this.imageHeight,
+    required this.childImageBuilder,
+    this.customBackgroundImageColor,
+    this.listViewCustomPadding,
+    this.customOnTapItem,
+  });
+
+  @override
+  ConsumerState<ListImageSection> createState() => _ListImageSectionState();
+}
+
+class _ListImageSectionState extends ConsumerState<ListImageSection> {
+  final StyleUtil _styleUtil = StyleUtil();
+  final ScrollController _scrollController = ScrollController();
+  final double _spacingItemWidthListView = 12;
+
+  int _selectedIndex = 0;
+  List<bool>? cardIsHovered;
+
+  @override
+  void initState() {
+    _scrollController.addListener(_onScrollActiveIndex);
+    cardIsHovered = List.generate(widget.images.length, (_) => false);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_onScrollActiveIndex);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _doSwapFocusedIndex(bool isToLeft){
+    if (isToLeft) {
+      if(_selectedIndex <= 0) return;
+      _selectedIndex--;
+    } else {
+      if(_selectedIndex >= widget.images.length - 1) return;
+      _selectedIndex++;
+    }
+    _scrollController.animateTo(
+      widget.imageWidth * _selectedIndex + (isToLeft ? 0 : _spacingItemWidthListView),
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOutQuad,
+    );
+  }
+
+  void _onScrollActiveIndex() {
+    int newIndex = (_scrollController.offset / widget.imageWidth).round();
+    // Ensure the last item is selected when the scroll reaches the end
+    if (_scrollController.position.atEdge) {
+      if (_scrollController.position.pixels > 0) {
+        newIndex = widget.images.length - 1;
+      }
+    }
+    if (newIndex != _selectedIndex) {
+      setState(() {
+        _selectedIndex = newIndex;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        SizedBox(
+          height: widget.listViewHeight,
+          child: ScrollConfiguration(
+            behavior: ScrollWithDragBehavior(),
+            child: ListView.separated(
+              controller: _scrollController,
+              // padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 5),
+              padding: widget.listViewCustomPadding,
+              addAutomaticKeepAlives: false,
+              cacheExtent: 100,
+              scrollDirection: Axis.horizontal,
+              itemCount: widget.images.length,
+              itemBuilder: (context, index) {
+                return _buildCardImage(index);
+              },
+              separatorBuilder: (context, index) => SizedBox(width: _spacingItemWidthListView),
+            ),
+          ),
+        ),
+        SizedBox(
+          width: MediaQuery.sizeOf(context).width,
+          height: widget.listViewHeight,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _listViewButtonController(
+                _selectedIndex != 0,
+                true,
+                getIsMobileSize(context) ? 40 : 80,
+                getIsMobileSize(context) ? 16 : 24,
+                    () => setState(() => _doSwapFocusedIndex(true)),
+              ),
+              _listViewButtonController(
+                _selectedIndex != widget.images.length - 1,
+                false,
+                getIsMobileSize(context) ? 40 : 80,
+                getIsMobileSize(context) ? 16 : 24,
+                    () => setState(() => _doSwapFocusedIndex(false)),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _listViewButtonController(bool buttonCondition, bool isButtonLeft, double buttonWidth, double iconSize, Function() onTapAction){
+    const Duration animationDuration = Duration(milliseconds: 150);
+    final Alignment beginGradientAlign = isButtonLeft ? Alignment.centerLeft : Alignment.centerRight;
+    final Alignment endGradientAlign = isButtonLeft ? Alignment.centerRight : Alignment.centerLeft;
+    final Color baseButtonColor = (ref.watch(isDarkMode) ? _styleUtil.c_33 : _styleUtil.c_255);
+    final Color baseIconColor = (ref.watch(isDarkMode) ? _styleUtil.c_238 : _styleUtil.c_61).withOpacity(.9);
+    final IconData mainIcon = isButtonLeft ? Icons.arrow_back_ios_new_rounded : Icons.arrow_forward_ios_rounded;
+
+    return AnimatedOpacity(
+      duration: animationDuration,
+      opacity: buttonCondition ? 1.0 : 0.0,
+      child: AnimatedContainer(
+        duration: animationDuration,
+        width: buttonCondition ? buttonWidth : 0,
+        child: InkWell(
+          onTap: onTapAction,
+          // splashColor: _styleUtil.c_170, // <-- Splash color
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          overlayColor: const WidgetStatePropertyAll(Colors.transparent),
+          child: Container(
+            height: double.maxFinite,
+            width: buttonWidth,
+            decoration: BoxDecoration(
+              border: Border(
+                left: isButtonLeft ? BorderSide(
+                  color: baseButtonColor,
+                ) : BorderSide.none,
+                right: !isButtonLeft ? BorderSide(
+                  color: baseButtonColor,
+                ) : BorderSide.none,
+              ),
+              gradient: LinearGradient(
+                begin: beginGradientAlign,
+                end: endGradientAlign,
+                colors: [
+                  baseButtonColor.withOpacity(1.0), // <-- Button color
+                  baseButtonColor.withOpacity(0.0),
+                ],
+              ),
+            ),
+            child: Center(
+              child: Icon(
+                mainIcon,
+                color: baseIconColor,
+                size: iconSize,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCardImage(int index){
+    return HighlightedWidgetOnHover(
+      widgetHeight: widget.imageHeight,
+      widgetWidth: widget.imageWidth,
+      onTapAction: () {
+        if (widget.customOnTapItem != null) widget.customOnTapItem!();
+        ref.read(isPreviewMode.notifier).state = index;
+      },
+      child: Container(
+        clipBehavior: Clip.antiAlias,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(getIsMobileSize(context) ? 0 : 8),
+          color:  (widget.customBackgroundImageColor != null) ? widget.customBackgroundImageColor! :
+          (ref.watch(isDarkMode))
+              ? _styleUtil.c_33
+              : _styleUtil.c_255,
+          boxShadow: [
+            BoxShadow(
+              color: (widget.customBackgroundImageColor != null) ? widget.customBackgroundImageColor! :
+                (ref.watch(isDarkMode))
+                  ? const Color.fromARGB(255, 200, 200, 200)
+                  : const Color.fromARGB(255, 233, 233, 233),
+              blurRadius: 7.0,
+            ),
+          ],
+        ),
+        height: widget.imageHeight,
+        width: widget.imageWidth,
+        child: widget.childImageBuilder(widget.images[index]),
+      ),
+    );
+  }
+}
