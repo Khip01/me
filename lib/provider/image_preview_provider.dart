8740a747875db6a030952ba375a948b3b2cd6b96
Khip01 <aakhifathallah24@gmail.com>
2024-07-20 22:14:43 +0700

    feat: added new provider state due to history detail page purposes
    index provider

--- lib/provider/image_preview_provider.dart
+++ lib/provider/image_preview_provider.dart
@@ -1,5 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+// for any image in listView
 final isPreviewMode = StateProvider<int?>(
+      (ref) => null,
+);
+
+// public varible for history detail page
+// (decide wich documentation index should be shown)
+final indexDocumentation = StateProvider<int?>(
       (ref) => null,
 );
