4ab08ec77baca1f8e9dab0e15dc1ff35160d0531
Khip01 <aakhifathallah24@gmail.com>
2024-07-26 19:47:15 +0700

    feat: remove unused provider

--- lib/provider/image_preview_provider.dart
+++ lib/provider/image_preview_provider.dart
@@ -1,10 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// for any image in listView
-final isPreviewMode = StateProvider<int?>(
-      (ref) => null,
-);
-
 // public varible for history detail page
 // (decide wich documentation index should be shown)
 final indexDocumentation = StateProvider<int?>(
