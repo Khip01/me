070bf34a9a543c86102711d3d43539f810a9ff69
Khip01 <aakhifathallah24@gmail.com>
2024-07-27 23:55:22 +0700

    refactor(provider): change StateProvider -> ChangeNotifierProvider in handling theme state

--- lib/provider/theme_provider.dart
+++ lib/provider/theme_provider.dart
@@ -1,5 +1,25 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final isDarkMode = StateProvider(
-        (ref) => false,
+// final isDarkMode = StateProvider(
+//         (ref) => false,
+// );
+
+// USING CHANGE NOTIFIER TO MANAGE THE THEME STATE
+class ThemeNotifier extends ChangeNotifier {
+  bool value = false;
+
+  void setThemeDark(){
+    value = true;
+    notifyListeners();
+  }
+
+  void setThemeLight(){
+    value = false;
+    notifyListeners();
+  }
+}
+
+final isDarkModeProvider = ChangeNotifierProvider<ThemeNotifier>(
+  (ref) => ThemeNotifier(),
 );
