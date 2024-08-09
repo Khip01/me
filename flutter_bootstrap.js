3a2b552351a040e310030180d75d27db1369b096
Khip01 <aakhifathallah24@gmail.com>
2024-08-09 13:25:55 +0700

    deploy: creation page
    fix header text theme bug

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3695247690"
+    serviceWorkerVersion: "1639828513"
   }
 });
