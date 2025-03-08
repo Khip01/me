3d13b4cd3ec71996f5052ec175a5725b7ba8e961
Khip01[desk] <aakhifathallah24@gmail.com>
2025-03-08 17:34:00 +0700

    deploy: reset to a specific commit
    ...due to a bug in the release

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1012579059"
+    serviceWorkerVersion: "3927975301"
   }
 });
