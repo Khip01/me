01d662fa74deaa61931a25b858d4a7398c46ef4d
Khip01 [Linux] <aakhifathallah24@gmail.com>
2025-08-21 15:06:27 +0700

    deploy: wasm fixed
    ~ switching from canvaskit to wasm for better performance

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4247238721"
+    serviceWorkerVersion: "3361211676"
   }
 });
