3d1b9b2c09cad274ff036efd1358c9874561bc63
Khip01 <aakhifathallah24@gmail.com>
2025-12-29 15:40:32 +0700

    deploy creation detail
    + fix: image preview button overlaping with back button
    + fix: image preview button UI & UX

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"site.webmanifest": "a9846ff2ab8658a079410e7f00864940",
-"main.dart.wasm": "7bc4fe7af4d5f3f6a99c08a57e69eae4",
+"main.dart.wasm": "3462ee7fd3e4037bd2b7039af9d9c83a",
 "manifest.json": "c4ba46dc44243c15d8608d8214d3fe8b",
 "main.dart.mjs": "a25a82d0bff19de0facc18a3d32c9659",
 "favicon-16x16.png": "3807d1ab0824f5ea43536edb57bfca49",
@@ -17,7 +17,7 @@ const RESOURCES = {"site.webmanifest": "a9846ff2ab8658a079410e7f00864940",
 "icons/Icon-192.png": "cb9887be9a24f48787c837ffb4dd41a2",
 "icons/Icon-maskable-192.png": "2d91e1a5a3f9e13ae381b358c0f33993",
 "browserconfig.xml": "a493ba0aa0b8ec8068d786d7248bb92c",
-"main.dart.js": "3b8925a405b9c8811c7f26a245fb343d",
+"main.dart.js": "7d17b568dde98900c32f1a6c18bd85ad",
 "canvaskit/chromium/canvaskit.js": "5e27aae346eee469027c80af0751d53d",
 "canvaskit/chromium/canvaskit.wasm": "24c77e750a7fa6d474198905249ff506",
 "canvaskit/chromium/canvaskit.js.symbols": "193deaca1a1424049326d4a91ad1d88d",
@@ -121,7 +121,7 @@ const RESOURCES = {"site.webmanifest": "a9846ff2ab8658a079410e7f00864940",
 "assets/assets/data/khip01-portfolio-default-rtdb-export.json": "4877f77e53ab8200ad11ce0013c449cb",
 "assets/AssetManifest.json": "bc0d3b7bc97f5c185f08e148ced8757e",
 "favicon.ico": "37d805c70cde765ba36478f63d153599",
-"flutter_bootstrap.js": "8b9624e51c99226ac8cbc5b69e3130bf",
+"flutter_bootstrap.js": "5c45a7401b02f67a1e13ca062f2e8bdc",
 "index.html": "29e8fff01448d875c8f6068202b15f79",
 "/": "29e8fff01448d875c8f6068202b15f79"};
 // The application shell files that are downloaded before a service worker can
