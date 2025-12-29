2ea5fe106f7711c3270218f4279d839065e94a6f
Khip01 <aakhifathallah24@gmail.com>
2025-12-29 11:56:19 +0700

    chore: updated CV!

--- lib/utility/link_util.dart
+++ lib/utility/link_util.dart
@@ -1,6 +1,6 @@
 class LinkUtil {
   static const String githubLink = 'https://github.com/Khip01';
-  static const String cvLink = 'https://drive.google.com/file/d/1JzxgGnv5oDYRo_Fam2nEaAVC6UeT3gJX/view?usp=sharing';
+  static const String cvLink = 'https://drive.google.com/file/d/1WG0Zhuk7Z0cDAH8kP0CxG7vPbgzh_EYd/view?usp=sharing';
   static const String instaLink = 'https://www.instagram.com/aakhifathallah?igsh=NXZwNDJ3eXlzamNu';
   static const String facebookLink = 'https://www.facebook.com/aakhif.athallah?mibextid=ZbWKwL';
   static const String gmailLink = 'https://mail.google.com/mail/?view=cm&fs=1&to=aakhifathallah24@gmail.com&su=Your_Subject&body=Your_Desc';
