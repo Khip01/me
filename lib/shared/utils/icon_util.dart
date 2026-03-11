51907577a4a76071e135de3ae04817d12d41cc2f
Khip01 <aakhifathallah24@gmail.com>
2026-03-11 23:29:54 +0700

    feat: convert assets image type to webp for better load time
    ~ remove several incognito assets and functionality

--- lib/shared/utils/icon_util.dart
+++ lib/shared/utils/icon_util.dart
@@ -3,34 +3,30 @@ class IconUtil {
   static const String root = 'assets/icons/';
 
   // General
-  static const String flutterLogo = "${root}flutter_16.png";
-  static const String firebaseLogo = "${root}firebase16.png"; // Currently not in use
-  static const String firebaseLogoNew = "${root}firebasenew16.png";
+  static const String flutterLogo = "${root}flutter_16.webp";
+  static const String firebaseLogo = "${root}firebase16.webp"; // Currently not in use
+  static const String firebaseLogoNew = "${root}firebasenew16.webp";
   // Welcome Page
-  static const String wavingHand = "${root}waving_hand.png";
+  static const String wavingHand = "${root}waving_hand.webp";
   // Creation Detail Page
-  static const String imgBrowserLight = "${root}browser/browser_light.png";
-  static const String imgBrowserDark = "${root}browser/browser_dark.png";
-  static const String imgLinkLight = "${root}link/link_light.png";
-  static const String imgLinkDark = "${root}link/link_dark.png";
+  static const String imgBrowserLight = "${root}browser/browser_light.webp";
+  static const String imgBrowserDark = "${root}browser/browser_dark.webp";
+  static const String imgLinkLight = "${root}link/link_light.webp";
+  static const String imgLinkDark = "${root}link/link_dark.webp";
   // Icons Social Media
-  static const String imgGithubDefault = "${root}github/github_default.png"; // Github
-  static const String imgGithubDark = "${root}github/github_hover_dark.png";
-  static const String imgGithubLight = "${root}github/github_hover_light.png";
-  static const String imgInstagramDefault = "${root}instagram/instagram_default.png"; // Instagram
-  static const String imgInstagramDark = "${root}instagram/instagram_hover_dark.png";
-  static const String imgInstagramLight = "${root}instagram/instagram_hover_light.png";
-  static const String imgFacebookDefault = "${root}facebook/facebook_default.png"; // Facebook
-  static const String imgFacebookDark = "${root}facebook/facebook_hover_dark.png";
-  static const String imgFacebookLight = "${root}facebook/facebook_hover_light.png";
-  static const String imgGmailDefault = "${root}gmail/gmail_default.png"; // Gmail
-  static const String imgGmailDark = "${root}gmail/gmail_hover_dark.png";
-  static const String imgGmailLight = "${root}gmail/gmail_hover_light.png";
-  static const String imgLinkedinDefault = "${root}linkedin/linkedin_default.png"; // LinkedIn
-  static const String imgLinkedinDark = "${root}linkedin/linkedin_hover_dark.png";
-  static const String imgLinkedinLight = "${root}linkedin/linkedin_hover_light.png";
-  // Super User Page
-  static const String incognitoMode = "${root}incognito_mode.png";
-  static const String incognitoFingerprint = "${root}incognito_fingerprint.png";
-  static const String incognitoMask = "${root}iconcognito_mask.png";
+  static const String imgGithubDefault = "${root}github/github_default.webp"; // Github
+  static const String imgGithubDark = "${root}github/github_hover_dark.webp";
+  static const String imgGithubLight = "${root}github/github_hover_light.webp";
+  static const String imgInstagramDefault = "${root}instagram/instagram_default.webp"; // Instagram
+  static const String imgInstagramDark = "${root}instagram/instagram_hover_dark.webp";
+  static const String imgInstagramLight = "${root}instagram/instagram_hover_light.webp";
+  static const String imgFacebookDefault = "${root}facebook/facebook_default.webp"; // Facebook
+  static const String imgFacebookDark = "${root}facebook/facebook_hover_dark.webp";
+  static const String imgFacebookLight = "${root}facebook/facebook_hover_light.webp";
+  static const String imgGmailDefault = "${root}gmail/gmail_default.webp"; // Gmail
+  static const String imgGmailDark = "${root}gmail/gmail_hover_dark.webp";
+  static const String imgGmailLight = "${root}gmail/gmail_hover_light.webp";
+  static const String imgLinkedinDefault = "${root}linkedin/linkedin_default.webp"; // LinkedIn
+  static const String imgLinkedinDark = "${root}linkedin/linkedin_hover_dark.webp";
+  static const String imgLinkedinLight = "${root}linkedin/linkedin_hover_light.webp";
 }
